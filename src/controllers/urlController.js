df47c3ad21f374d20980107c595a31d3d665cf8b
unknown <kumarabhishek0947@gmail.comgit config --global user.name AAbhishek18>
2022-05-20 03:36:00 +0530

    Project-4 url Shortner done

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -124,7 +124,7 @@ const getUrl = async function (req, res) {
                 return res.status(302).redirect(findUrl.longUrl);
 
             } else {
-                return res.status(404).send({ status: false, messege: "Cant Find What You Are Looking For" })
+                return res.status(404).send({ status: false, messege: "Url not found" })
             }
 
         }
